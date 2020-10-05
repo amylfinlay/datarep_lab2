c133a60499378a19cda0eaaca66e4f19a6473bc3
amylfinlay <amylfinlay@gmail.com>
2020-10-05 17:56:01 +0100

    First Commit

---
+++ bmi.js
@@ -0,0 +1,20 @@
+class BMI { //A blueprint for creating projects
+    constructor(height, weight) { //Starting point of the class, first method called
+        this.height = height; //Arugments height and weight
+        this.weight = weight;
+
+    }
+
+    calBMI() { //Method in which BMI will calculated
+
+        let bmi = this.weight / (this.height ** 2);
+        return bmi;
+    }
+
+}
+
+//Instance which will invoke the method calculate BMI
+let MyBmi = new BMI (2, 90);
+let calBMI = MyBmi.calBMI();
+
+console.log(calBMI) //Will call output
